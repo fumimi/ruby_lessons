51e41b656dea9ac7745438aa29bd1cdd8fbdfa74
fumimi <info@fumimi.jp>
2016-11-18 08:20:51 +0000

    よく使うメソッドを追加しました。

--- array/array.rb
+++ array/array.rb
@@ -17,6 +17,10 @@
 # 集合の積演算です。両方の配列に含まれる要素からなる新しい配列を返 します。重複する要素は取り除かれます。
 # p [1, 1, 2, 3] & [1, 3, 4] #=> [1, 3]
 
+################################################################################
+
+# 参考サイト
+# http://qiita.com/shu_0115/items/5f08a6d58d1acc46fbad#array1
 
 # 1 nillを取り除いた配列を返す
 # array = [1, nil, 2, nil, 3, nil].compact # => [1, 2, 3]
@@ -26,12 +30,10 @@
 # array = [1, 2, 3].concat([4, 5, 6]) # => [1, 2, 3, 4, 5, 6]
 # p array
 
-
 # 2 指定した値と等しい全て削除する
 # a = [1, 2, 3, 4, 1, 2]
 # a.delete(1)
 # p a
-
 
 # 3 条件が真になった要素を削除する
 # a = [0, 1, 2, 3, 4, 5]
@@ -73,9 +75,86 @@
 # p array.shuffle
 
 # 11 指定した部分から抽出した部分配列を返す
-array = [1, 2, 3, 4, 5]
-p array.slice(0, 3)
-p array.alice(2..4)
+# array = [1, 2, 3, 4, 5]
+# p array.slice(0, 3)
+# p array.alice(2..4)
+
+# 12 配列を整理して返す
+# array = ['e', 'd', 'a', 'b', 'c']
+# p array.sort
+
+# 13 重複を除いた配列を返す
+# array = [2, 1, 5, 1, 3, 2, 4, 3]
+# p array.uniq
+
+################################################################################
+
+
+# list = ["a", "b", "c", "d"]
+# for i in 0..3
+#     print i + 1, "番目の要素は", list[i], "です。\n"
+# end
+
+# list = [1, 3, 5, 7, 9]
+# sum = 0
+# for i in 0..4
+#     sum += list[i]
+# end
+# print "合計：", sum, "\n"
+
+# list = [1, 3, 5, 7, 9]
+# sum = 0
+# list.each do |elem|
+#     sum += elem
+# end
+# print "合計：", sum , "\n"
+
+# list = ["a", "b", "c", "d"]
+# list.each_with_index do |elem, i|
+#     print i + 1, "番目の要素は", elem, "です。\n"
+# end
+
+# array1 = [1, 2, 3, 4, 5]
+# array2 = [10, 20, 30, 40, 50]
+# array3 = [100, 200, 300, 400, 500]
+# i = 0
+# result = []
+# while i < array1.length
+#     result << array1[i] + array2[i] + array3[i]
+#     i += 1
+# end
+# p result
+
+array1 = [1, 2, 3, 4, 5]
+array2 = [10, 20, 30, 40, 50]
+array3 = [100, 200, 300, 400, 500]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
